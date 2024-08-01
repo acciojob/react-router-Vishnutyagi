ba0eeb56c141950df27e40be9a94cb2dd91cdb14
Vishnu tyagi <vishnu.tyagi.mec20@itbhu.ac.in>
2024-08-01 12:35:46 +0530

    final

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ function Header() {
     <div>
         <ul>
             <li><Link to='/'>Home</Link></li>
-            <li><Link to='/about'>about</Link></li>
+            <li><Link to='/about'>About</Link></li>
         </ul>
     </div>
   )
